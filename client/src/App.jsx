f833694bd7fac5ade88ba2d717e1fdb3a2c95927
Deepak Kumar <deepakk.ug24.me@nitp.ac.in>
2026-06-06 11:30:32 +0530

    Let's start bullding this owsome project

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const App = () => {
+  return (
+    <div>App
+      <h1 style={{ color: 'blue' }}>Welcome to OptiWork Systems</h1>
+    </div>
+  )
+}
+export default App;
+
